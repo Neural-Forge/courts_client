9b0ae300c1ee0ad349a124cb9b82a50caf5a6d0d
Enock <enock.mokua@student.moringaschool.com>
2023-03-06 05:53:16 +0300

    render button conditionally

--- src/pages/chat.js
+++ src/pages/chat.js
@@ -1,5 +1,21 @@
 import "./chat.css";
+import React, { useState } from "react";
 
 export default function Chat() {
-  return <section>This is the chat component</section>;
+  let [show, setShow] = useState(false);
+
+  function handleClik() {
+    if (show === true) {
+      setShow(false);
+    } else {
+      setShow(true);
+    }
+  }
+  
+  return (
+    <section>
+      <button onClick={handleClik}>Show element</button>
+      {show && <div>This is the element to show.</div>}
+    </section>
+  );
 }
