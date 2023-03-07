40850f12dee8b2bc93482fc93c7bbcdb9f1b9add
Enock <enock.mokua@student.moringaschool.com>
2023-03-07 17:15:38 +0300

    create layout of chat container

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <main>This is the home page</main>;
+  return (
+    <main className="chat-container">
+      <p className="sender"></p>
+      <p className="receiver"></p>
+      <form>
+        <input type="text" placeholder="Enter message here" />
+        <button type="submit">Send</button>
+      </form>
+    </main>
+  );
 }
