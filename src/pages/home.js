0d3fc52d90e86eafa3c5480e4c9acb4551747348
Enock <enock.mokua@student.moringaschool.com>
2023-03-07 17:49:36 +0300

    create structure of messages container and add basic styling

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,13 @@
 export default function Home() {
   return (
     <main className="chat-container">
-      <p className="sender"></p>
-      <p className="receiver"></p>
-      <form>
+      <div className="sender">
+        <p>I have sent a message</p>
+      </div>
+      <div className="receiver">
+        <p>I have received the message jsjsj sj sjsjsj jsjs jsj kssks keldede lalkasnks </p>
+      </div>
+      <form id="chat-form">
         <input type="text" placeholder="Enter message here" />
         <button type="submit">Send</button>
       </form>
